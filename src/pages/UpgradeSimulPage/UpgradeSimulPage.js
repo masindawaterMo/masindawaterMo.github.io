29ebd586d2e701f26cd66bdf935c617874882030
김지홍 <kjh95044@naver.com>
2024-10-12 01:03:22 +0900

    랭킹 1,2,3등 색깔 및 일일랭킹 추가

--- src/pages/UpgradeSimulPage/UpgradeSimulPage.js
+++ src/pages/UpgradeSimulPage/UpgradeSimulPage.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import UpgradeItemCard from "../../components/Card/UpgradeItemCard/UpgradeItemCard";
 import { initializeApp } from "firebase/app";
+import { Timestamp } from "firebase/firestore";
 import {
   getFirestore,
   collection,
@@ -11,12 +12,14 @@ import {
   doc,
   setDoc,
   addDoc,
+  where,
 } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 import styles from "./UpgradeSimulPage.module.css";
 
 const UpgradeSimulPage = () => {
   const [rankingList, setRankingList] = useState([]); // 랭킹 데이터를 저장할 상태
+  const [todayRankingList, setTodayRankingList] = useState([]); // 랭킹 데이터를 저장할 상태
   const [loading, setLoading] = useState(true); // 로딩 상태 관리
 
   // Firebase 구성 객체 (필요에 맞게 수정)
@@ -34,13 +37,41 @@ const UpgradeSimulPage = () => {
   const db = getFirestore(app);
   const analytics = getAnalytics(app);
 
-  // 랭킹을 가져오는 함수
+  // 누적 랭킹을 가져오는 함수
   const getRanking = async () => {
     const rankingsRef = collection(db, "rankings");
     const rankingQuery = query(
       rankingsRef,
       orderBy("upgradeCount", "desc"),
-      orderBy("timestamp", "desc")
+      orderBy("timestamp", "desc"),
+      limit(10)
+    );
+
+    const querySnapshot = await getDocs(rankingQuery);
+    const rankingList = [];
+    querySnapshot.forEach((doc) =>
+      rankingList.push({ id: doc.id, ...doc.data() })
+    );
+
+    return rankingList;
+  };
+
+  // 오늘 날짜의 시작과 끝을 구하는 함수
+  const startOfToday = new Date();
+  startOfToday.setHours(0, 0, 0, 0); // 오늘의 시작 시간 (00:00:00)
+  const endOfToday = new Date();
+  endOfToday.setHours(23, 59, 59, 999); // 오늘의 끝 시간 (23:59:59)
+
+  // 일일 랭킹을 가져오는 함수
+  const getTodayRanking = async () => {
+    const rankingsRef = collection(db, "rankings");
+    const rankingQuery = query(
+      rankingsRef,
+      where("timestamp", ">=", Timestamp.fromDate(startOfToday)),
+      where("timestamp", "<=", Timestamp.fromDate(endOfToday)),
+      orderBy("upgradeCount", "desc"),
+      orderBy("timestamp", "desc"),
+      limit(10)
     );
 
     const querySnapshot = await getDocs(rankingQuery);
@@ -58,7 +89,9 @@ const UpgradeSimulPage = () => {
       try {
         setLoading(true); // 데이터 로딩 시작
         const rankingData = await getRanking();
+        const todayRankingData = await getTodayRanking();
         setRankingList(rankingData); // 랭킹 데이터 업데이트
+        setTodayRankingList(todayRankingData);
       } catch (error) {
         console.error("Error fetching rankings:", error);
       } finally {
@@ -72,26 +105,69 @@ const UpgradeSimulPage = () => {
   return (
     <div id="wrap">
       <h1>강화 시뮬레이터</h1>
-      <div className={styles["card-center"]}>
-        <div className={styles["left"]}>
-          <h1 className={styles["left-title"]}>단축키</h1>
-          <h2>Q : 강화</h2>
-          <h2>R : 리셋</h2>
-        </div>
-        <UpgradeItemCard rankingList={rankingList} />
-        <div className={styles["right"]}>
-          <h1>Top 10</h1>
-          {loading ? (
-            <p>Loading...</p> // 데이터 로딩 중일 때 표시
-          ) : (
-            <ul className={styles["rank-ul"]}>
-              {rankingList.map((rank, index) => (
-                <li key={rank.id} className={styles["rank-li"]}>
-                  {index + 1}. {rank.nickname} - {rank.upgradeCount}강
-                </li>
-              ))}
-            </ul>
-          )}
+      <div className="center">
+        <div className={styles["upgrade-container"]}>
+          <div className={styles["left"]}>
+            <h1 className={styles["left-title"]}>단축키</h1>
+            <h2>Q : 강화</h2>
+            <h2>R : 리셋</h2>
+          </div>
+          <UpgradeItemCard rankingList={rankingList} />
+          <div className={styles["right"]}>
+            <h1>일일 Top 10</h1>
+            {loading ? (
+              <p>Loading...</p> // 데이터 로딩 중일 때 표시
+            ) : (
+              <ul className={styles["rank-ul"]}>
+                {todayRankingList.map((rank, index) => (
+                  <li
+                    key={rank.id}
+                    className={styles["rank-li"]}
+                    style={{
+                      color:
+                        index === 0
+                          ? "gold"
+                          : index === 1
+                          ? "#CF61F3"
+                          : index === 2
+                          ? "#cd7f32"
+                          : "white",
+                    }}
+                  >
+                    {index + 1}. {rank.nickname} - {rank.upgradeCount}강
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
+
+          <div className={styles["right"]}>
+            <h1>누적 Top 10</h1>
+            {loading ? (
+              <p>Loading...</p> // 데이터 로딩 중일 때 표시
+            ) : (
+              <ul className={styles["rank-ul"]}>
+                {rankingList.map((rank, index) => (
+                  <li
+                    key={rank.id}
+                    className={styles["rank-li"]}
+                    style={{
+                      color:
+                        index === 0
+                          ? "gold"
+                          : index === 1
+                          ? "#CF61F3"
+                          : index === 2
+                          ? "#cd7f32"
+                          : "white",
+                    }}
+                  >
+                    {index + 1}. {rank.nickname} - {rank.upgradeCount}강
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
         </div>
       </div>
     </div>
